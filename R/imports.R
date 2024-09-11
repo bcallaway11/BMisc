566b0a02de10d3216ab2cf549285dcb6a951a751
Brantly Callaway <brantly.callaway@uga.edu>
2024-09-11 15:37:34 -0400

    summarize check results

--- R/imports.R
+++ R/imports.R
@@ -6,7 +6,8 @@
 #' @importFrom Rcpp sourceCpp
 #' @import graphics
 #' @import stats
-#' @importFrom dplyr group_by mutate %>% group_map
+#' @importFrom dplyr group_by mutate %>% group_map select starts_with
+#' @importFrom tidyr pivot_wider
 #' @importFrom caret findLinearCombos
 "_PACKAGE"
 
